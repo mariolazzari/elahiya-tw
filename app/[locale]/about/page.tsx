18a7045206ebc311e60ca7ad70471382ba9c0151
Mario Lazzari <mario.lazzari@gmail.com>
2023-04-09 23:20:05 +0200

    feat: about page

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -1,3 +1,4 @@
+import { useTranslations } from "next-intl";
 import CardBox from "@/components/CardBox";
 
 export const metadata = {
@@ -6,12 +7,14 @@ export const metadata = {
 };
 
 const About = () => {
+  const t = useTranslations("About");
+
   const renderImages = () =>
     [1, 2, 3].map(id => (
       <CardBox
         key={id}
-        title="title"
-        description="description"
+        title={t(`card${id}Title`)}
+        description={t(`card${id}Text`)}
         imagePath={`/images/card${id}.png`}
       />
     ));
@@ -19,7 +22,10 @@ const About = () => {
   return (
     <div className="flex flex-col items-center justify-center w-full min-h-[calc(100vh-100px)] p-2 overflow-auto bg-gradient-to-br from-violet-300 via-indigo-300 to-purple-300">
       <div className="p-2 my-4 rounded bg-violet-100">
-        <h1 className="text-xl font-semibold text-indigo-800">About</h1>
+        <h2 className="mb-4 text-xl font-semibold text-center text-indigo-800">
+          {t("title")}
+        </h2>
+        <p className="max-w-md text-justify">{t("text")}</p>
       </div>
 
       <div className="flex flex-wrap items-center justify-center gap-4">
--- components/CardBox/index.tsx
+++ components/CardBox/index.tsx
@@ -9,10 +9,12 @@ const CardBox = ({
   imagePath,
 }: CardBoxProps) => {
   return (
-    <div className="flex flex-col justify-center rounded shadow-md item-center shadow-violet-400 bg-indigo-50">
+    <div className="w-[380px] flex flex-col justify-center rounded shadow-md item-center shadow-violet-400 bg-indigo-50">
       <ImageBox src={imagePath} alt={title} width={width} height={height} />
       <h6 className="m-2 text-2xl text-purple-700">{title}</h6>
-      <p className="m-2 text-indigo-600 text-md">{description}</p>
+      <p className="h-[160px] m-2 text-indigo-600 text-md text-justify">
+        {description}
+      </p>
     </div>
   );
 };
