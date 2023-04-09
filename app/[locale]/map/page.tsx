93453279effc3a19dea601ba74eae094b3397c6f
Mario Lazzari <mario.lazzari@gmail.com>
2023-04-09 23:08:01 +0200

    feat: added locales

--- app/[locale]/map/page.tsx
+++ app/[locale]/map/page.tsx
@@ -1,8 +1,12 @@
+import { useTranslations } from "next-intl";
 import MapBox from "@/components/MapBox";
 
 const Map = () => {
+  const t = useTranslations("Map");
+
   return (
-    <div className="flex items-center justify-center w-full h-full bg-gradient-to-tr from-violet-200 to-indigo-200">
+    <div className="flex flex-col items-center justify-center w-full h-full bg-gradient-to-tr from-violet-200 to-indigo-200">
+      <h2 className="mb-4 text-4xl text-indigo-700">{t("title")}</h2>
       <MapBox />
     </div>
   );
