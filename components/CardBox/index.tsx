8664a175e01bb80d2bb233d82b9b9e3c74b4fed5
Mario Lazzari <mario.lazzari@gmail.com>
2023-04-10 11:26:06 +0200

    fix: images width

--- components/CardBox/index.tsx
+++ components/CardBox/index.tsx
@@ -4,15 +4,15 @@ import ImageBox from "../ImageBox";
 const CardBox = ({
   title,
   description,
-  width = 380,
+  width = 350,
   height = 300,
   imagePath,
 }: CardBoxProps) => {
   return (
-    <div className="w-[380px] flex flex-col justify-center rounded shadow-md item-center shadow-violet-400 bg-indigo-50">
+    <div className="w-[350px] flex flex-col justify-center rounded shadow-md item-center shadow-violet-400 bg-gradient-to-br from-purple-100 via-white to-violet-100">
       <ImageBox src={imagePath} alt={title} width={width} height={height} />
-      <h6 className="m-2 text-2xl text-purple-700">{title}</h6>
-      <p className="h-[160px] m-2 text-indigo-600 text-md text-justify">
+      <h6 className="mt-2 ml-2 text-2xl text-purple-800">{title}</h6>
+      <p className="h-[180px] p-2 text-indigo-800 text-md text-justify">
         {description}
       </p>
     </div>
--- components/ImageBox/index.tsx
+++ components/ImageBox/index.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 import ImageBoxProps from "./ImageBoxProps";
 
 const ImageBox = ({ src, alt, width, height }: ImageBoxProps) => {
-  const getStyle = () => `relative w-[${width}px] h-[${height}px]`;
-
   return (
-    <div className={getStyle()}>
+    <div className={`relative w-[${width}px] h-[${height}px]`}>
       <Image
         className="object-cover"
         src={src}
