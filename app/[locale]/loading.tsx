8b3ea47c3d74e7eab9ea3259e5d4e4839add56a3
Mario Lazzari <mario.lazzari@gmail.com>
2023-04-10 12:09:24 +0200

    refact: loading page

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -2,8 +2,8 @@ import { ImSpinner3 } from "react-icons/im";
 
 const Loading = () => {
   return (
-    <div className="flex items-center justify-center w-screen h-screen">
-      <ImSpinner3 className="animate-spin" color="purple" size={64} />
+    <div className="flex items-center justify-center w-screen h-screen bg-gradient-to-br from-violet-200 via-indigo-200 to-purple-200">
+      <ImSpinner3 className="text-purple-600 animate-spin" size={64} />
     </div>
   );
 };
