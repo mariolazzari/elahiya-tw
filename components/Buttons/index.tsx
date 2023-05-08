b353aa4549a4468668a563f0f9da0df2142a6c90
Mario Lazzari <mario.lazzari@gmail.com>
2023-05-08 20:03:36 +0200

    refact: button

--- components/Buttons/index.tsx
+++ components/Buttons/index.tsx
@@ -1,17 +1,18 @@
 import Link from "next/link";
 
 const Buttons = () => {
-  const style = "w-32 px-5 py-2 text-xl text-white rounded-xl";
+  const style =
+    "w-32 px-5 py-2 text-xl text-white rounded-full active:scale-90";
   const buttons = [
     {
       link: "/about",
       label: "About us",
-      style: `${style} bg-purple-700 rounded-xl hover:bg-purple-600 active:bg-purple-800`,
+      style: `${style} bg-purple-700 hover:bg-purple-600 active:bg-purple-800`,
     },
     {
       link: "/map",
       label: "Find us",
-      style: `${style} bg-indigo-700 active:bg-indigo-800 hover:bg-indigo-600 rounded-xl`,
+      style: `${style} bg-indigo-700 active:bg-indigo-800 hover:bg-indigo-600`,
     },
   ];
 
