feecb637bce5cfe1d4f526d085b90198ca0e9fd1
Mario Lazzari <mario.lazzari@gmail.com>
2023-05-29 20:37:27 +0200

    refact: map

--- components/MapBox/index.tsx
+++ components/MapBox/index.tsx
@@ -9,23 +9,23 @@ const MapBox = () => {
   const apiKey = "EGswGwC8sNX2v1gMT662";
   const zoom = 14;
 
-  const init = { longitude, latitude, zoom };
-
   return (
-    <div>
-      <MapGl
-        mapLib={maplibregl}
-        initialViewState={init}
-        style={{
-          width: "90vw",
-          height: "60vh",
-        }}
-        mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`}
-      >
-        <NavigationControl position="top-left" />
-        <Marker latitude={latitude} longitude={longitude} color="purple" />
-      </MapGl>
-    </div>
+    <MapGl
+      mapLib={maplibregl}
+      initialViewState={{
+        longitude,
+        latitude,
+        zoom,
+      }}
+      style={{
+        width: "90vw",
+        height: "60vh",
+      }}
+      mapStyle={`https://api.maptiler.com/maps/streets/style.json?key=${apiKey}`}
+    >
+      <NavigationControl position="top-left" />
+      <Marker latitude={latitude} longitude={longitude} color="purple" />
+    </MapGl>
   );
 };
 
