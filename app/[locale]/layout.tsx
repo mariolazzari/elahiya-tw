1f4c70266b99b350cbf82d24b71ffd481ecdac7d
Mario Lazzari <mario.lazzari@gmail.com>
2024-02-21 20:37:15 +0100

    refact: google analytics

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 import LayoutProps from "@/types/LayoutProps";
 import MenuBar from "@/components/MenuBar";
 import Footer from "@/components/Footer";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import { Metadata } from "next";
+import { GoogleAnalytics } from "@next/third-parties/google";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -17,24 +17,21 @@ export const metadata: Metadata = {
   title: "Elahiya Pro Language",
   description:
     "Elahiya Pro Language school has been providing English languages, TOEFL, IELTS and French classes in Rudehen for 20 years.It is located at the 6th Bustan",
-  // themeColor: "#553c9a",
 };
 
 const RootLayout = ({ children }: LayoutProps) => {
   const locale = useLocale();
-  const gaid = process.env.GA_TRACKING_ID as string;
+  const gaid = process.env.GA_TRACKING_ID!;
 
   return (
     <html lang={locale}>
-      <head>
-        <GoogleAnalytics id={gaid} />
-      </head>
-
       <body className={`${inter.variable} font-sans`}>
         <MenuBar />
         <div className="h-[calc(100vh-100px)] overflow-y-auto">{children}</div>
         <Footer />
       </body>
+
+      <GoogleAnalytics gaId={gaid} />
     </html>
   );
 };
