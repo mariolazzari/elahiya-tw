e412fed7208a215c444279b4c044388591cf699d
Mario Lazzari <mario.lazzari@gmail.com>
2024-05-11 00:02:33 +0200

    refact: drawer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,13 +1,15 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <footer className="h-[50px] bg-gradient-to-br from-violet-400 via-indigo-400 to-purple-400 flex  justify-center items-center">
-      <a
+      <Link
         className="font-semibold text-white text-md hover:scale-110"
         href="https://mariolazzari.it"
         target="_blank"
       >
         Mario Lazzari ©{new Date().getFullYear()}
-      </a>
+      </Link>
     </footer>
   );
 };
